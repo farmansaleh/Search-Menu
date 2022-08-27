088cb3d19dcd26d925c9421c912a0ca46e2068c8
Farman <pubgplayers95@gmail.com>
2022-08-27 23:20:44 +0530

    add date

--- js/SearchMenu.js
+++ js/SearchMenu.js
@@ -1,48 +1,52 @@
+/*
+    author : @Farman Saleh
+ 	date   : August 27/2022
+ 	github : github.com/farmansaleh
+**/
+var allMenu="";
+var listData="";
+$(document).ready(function() {
+    //get All menu list
+    listData=$("#searchMenuList");
+    //get all menu html code
+    allMenu=$("#searchMenuList")[0].innerHTML;
 
-    var allMenu="";
-    var listData="";
-    $(document).ready(function() {
-        //get All menu list
-        listData=$("#searchMenuList");
-        //get all menu html code
-        allMenu=$("#searchMenuList")[0].innerHTML;
+    //fire event on key up
+    $("#searchMenu").on("keyup", function() 
+    {
+        var matchCount=0;
+        //get menu name which you want to search and convert all character into lowercase
+        var menuName = $(this).val().toLowerCase();
+        if($.trim(menuName)  != null && $.trim(menuName) != ""){
 
-        //fire event on key up
-        $("#searchMenu").on("keyup", function() 
-        {
-            var matchCount=0;
-            //get menu name which you want to search and convert all character into lowercase
-            var menuName = $(this).val().toLowerCase();
-            if($.trim(menuName)  != null && $.trim(menuName) != ""){
-
-                $("#searchMenuList li").filter(function() 
-                {
-                    //first check that menu is in which position locat and get the index position
-                    if($(this).toggle($(this).text().toLowerCase().indexOf(menuName) != -1)) {
-                        
-                        //if menu index position is greater than -1 then it will show otherwise it will remove
-                        if($(this).text().toLowerCase().indexOf(menuName) > -1) {
-                            $(this).toggle($(this).text().toLowerCase().indexOf(menuName) > -1);
-                            $(this).addClass("active");
-                            $(this).find("ul").css("display","block");
-                            matchCount++;
-                        }
-                        else {
-                            $(this).toggle($(this).text().toLowerCase().indexOf(menuName) > -1);
-                        }
+            $("#searchMenuList li").filter(function() 
+            {
+                //first check that menu is in which position locat and get the index position
+                if($(this).toggle($(this).text().toLowerCase().indexOf(menuName) != -1)) {
+                    
+                    //if menu index position is greater than -1 then it will show otherwise it will remove
+                    if($(this).text().toLowerCase().indexOf(menuName) > -1) {
+                        $(this).toggle($(this).text().toLowerCase().indexOf(menuName) > -1);
+                        $(this).addClass("active");
+                        $(this).find("ul").css("display","block");
+                        matchCount++;
                     }
-                });
-                if(matchCount == 0) {
-                    $("#div_menu_id").find(".noMenuFound").show();
+                    else {
+                        $(this).toggle($(this).text().toLowerCase().indexOf(menuName) > -1);
+                    }
                 }
-                else {
-                    $(".noMenuFound").hide();
-                }
+            });
+            if(matchCount == 0) {
+                $("#div_menu_id").find(".noMenuFound").show();
             }
             else {
-                $("#searchMenuList").empty();
                 $(".noMenuFound").hide();
-                $("#searchMenuList").append(allMenu);
             }
-        });
+        }
+        else {
+            $("#searchMenuList").empty();
+            $(".noMenuFound").hide();
+            $("#searchMenuList").append(allMenu);
+        }
     });
+});
